37fae97e2250b69627275df49f45925d6e9911f2
h1-the-swan <jason.portenoy@gmail.com>
2018-01-26 23:44:39 -0800

    WIP resume

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,22 @@
-from flask import render_template
+import os, json
+from flask import render_template, url_for, flash
 # from ..models import EditableHTML
 
 from . import main
+from app import basedir
 
 
 @main.route('/')
 def index():
     return render_template('main/index.html')
+
+@main.route('/resume')
+def resume():
+
+    resume_data_fname = basedir + url_for('main.static', filename='data/resume/resume-data.json')
+    with open(resume_data_fname, 'r') as f:
+        resume_data = json.load(f)
+    return render_template('main/resume.html', resume_data=resume_data)
 
 
 # @main.route('/about')
